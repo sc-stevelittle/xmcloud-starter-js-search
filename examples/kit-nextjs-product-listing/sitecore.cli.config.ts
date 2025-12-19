51cb6a84a2a00ccb72741d6d2df0858baed898cd
Chathumika Alwis <ctlw@sitecore.net>
2025-12-19 15:22:16 +0530

    Revert "Introduce Search Experience component (#220)"
    This reverts commit 8a12c50de8a533791ac3c1cb9d849f7a81975b39.

--- examples/kit-nextjs-product-listing/sitecore.cli.config.ts
+++ examples/kit-nextjs-product-listing/sitecore.cli.config.ts
@@ -22,15 +22,6 @@ export default defineCliConfig({
   componentMap: {
     paths: ['src/components'],
     // Exclude content-sdk auxillary components
-    exclude: [
-      'src/components/content-sdk/*',
-      'src/components/ui/*',
-      'src/components/lib/*',
-      'src/components/video/*',
-      'src/components/multi-promo/*',
-      'src/components/image-carousel/*',
-      'src/components/accordion-block/*',
-      'src/components/search-experience/search-components/**/*',
-    ],
+    exclude: ['src/components/content-sdk/*', 'src/components/ui/*', 'src/components/lib/*', 'src/components/video/*', 'src/components/multi-promo/*', 'src/components/image-carousel/*', 'src/components/accordion-block/*'],
   },
 });
